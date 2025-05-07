ba49953e094589d80195d8e9a0d8b099ff0aec54
Daniel Brandt <daniel.gordon.brandt@gmail.com>
2025-05-07 12:33:51 +0200

    basic tests complete

--- test_product.py
+++ test_product.py
@@ -23,3 +23,14 @@ def test_product_becomes_inactive_at_zero_quantity():
     p = Product("Test", price=100, quantity=1)
     p.buy(1)
     assert not p.is_active()
+
+def test_purchase_updates_quantity():
+    p = Product("Test", price=100, quantity=2)
+    res = p.buy(1)
+    assert res == 100
+    assert p.quantity == 1
+
+def test_buy_too_much():
+    p = Product("Test", price=100, quantity=10)
+    with pytest.raises(ValueError):
+        p.buy(11)
